f098dff1cf6e25c23d1daa39b6248efb48b9a133
webdown404 <que.babault@gmail.com>
2021-02-16 17:45:22 +0100

    feat: add frontend

--- .github/actions/deploy-customization/entrypoint.sh
+++ .github/actions/deploy-customization/entrypoint.sh
@@ -8,6 +8,7 @@ echo "Kubernetes config files customization..."
 
 cd ${deploy_directory}/overlays/${stage}
 $(kustomize edit set image eu.gcr.io/aperturedevs/sob-api-php:${commit_hash})
+$(kustomize edit set image eu.gcr.io/aperturedevs/sob-frontend-webserver:${commit_hash})
 kustomize build -o ./../../manifest.yaml
 echo ::set-output name=manifest-path::${deploy_directory}/manifest.yaml
 echo "Kubernetes config files customization done"
