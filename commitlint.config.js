6498737991ce40a61ae40a0907544225cecfdda2
webdown404 <que.babault@gmail.com>
2021-12-15 18:39:42 +0100

    infra: update custom commitlint types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,22 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'infra'
+      ],
+    ],
+  }
+}
